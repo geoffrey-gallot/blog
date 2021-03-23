e4cb993e4e1c824a0bad1471a6ff0f784b86e3f9
geoffrey-gallot <geoffrey.gallot@gmail.com>
2021-03-23 18:12:46 +0100

    mise en place owl carousel

--- scr/js/app.js
+++ scr/js/app.js
@@ -1,32 +1,48 @@
-let meteo = document.getElementById('meteo');
-let urlMeteo = 'http://api.openweathermap.org/data/2.5/weather?q=Angouleme,fr&units=metric&lang=fr&appid=8fdfe3c8a43425543990416ac7b27c83';
+let meteo = document.getElementById("meteo");
+let urlMeteo =
+    "http://api.openweathermap.org/data/2.5/weather?q=Angouleme,fr&units=metric&lang=fr&appid=8fdfe3c8a43425543990416ac7b27c83";
 
-
-let result = document.getElementById('result');
-let urlAPI = 'https://my-blog-content-manage.herokuapp.com/Posts';
+let result = document.getElementById("result");
+let urlAPI = "https://my-blog-content-manage.herokuapp.com/Posts";
 
 fetch(urlAPI)
     // renvoi un type json
-    .then(res => res.json())
+    .then((res) => res.json())
     //renvoi les données de l API pour l affichage
-    .then(datas =>
-        datas.forEach(data => {
-            let dateSplit = (data.published_at.split('T'))[0];
+    .then((datas) =>
+        datas.forEach((data) => {
+            let dateSplit = data.published_at.split("T")[0];
             let retourPost = `${data.Title}
                               ${dateSplit}
                               ${data.Description}`;
-            let li = document.createElement('li');
+            let li = document.createElement("li");
             result.prepend(li);
             li.innerText = retourPost;
-        }
-    ));
- 
+        })
+    );
+
 //recuperation données API open weather
 fetch(urlMeteo)
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
         meteo.innerHTML = `${data.name}<br>
                                ${data.main.temp}°<br>
                                ${data.weather[0].description}`;
     });
 
+//configuration owl carousel
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 50,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+});
+$(".play").on("click", function () {
+    owl.trigger("play.owl.autoplay", [1000]);
+});
+$(".stop").on("click", function () {
+    owl.trigger("stop.owl.autoplay");
+});
